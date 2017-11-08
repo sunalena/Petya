fcbedfd49e5d21bc75ed8772b65e9524ca324f1e
user <user@mail.com>
2017-11-08 08:39:00 +0200

    first commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        public bool funkch(int a, int b, int c)
+        {
+            if(a == 0)
+            {
+                throw new ArgumentException();
+            }
+            if (b * b - 4 * a * c >= 0)
+                return true;
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var p = new Program();
+            var r = p.funkch(1, 2, 3);
+            Assert.AreEqual(r, false);
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestMethod2()
+        {
+            var p = new Program();
+            var r = p.funkch(0, 2, 3);
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var p = new Program();
+            var r = p.funkch(1, 2, 3);
+            Assert.IsFalse(r);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var p = new Program();
+            var r = p.funkch(1, 2, 3);
+            Assert.IsTrue(!r);
+        }
+    }
+}
